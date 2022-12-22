11d4e3b3ce5f611470d7358a4f6b731eac5b84d3
Xelhua Corona <xelcoronap@gmail.com>
2022-12-22 14:01:17 -0600

    explore sub folders

--- pages/api/version.js
+++ pages/api/version.js
@@ -3,5 +3,9 @@ import fs from "fs";
 export default async function handler(req, res) {
   // version: await gs.version()
 
-  res.status(200).json({ exists: fs.readdirSync("./") });
+  const files = fs
+    .readdirSync("./")
+    .map((file) => file + "/" + fs.readdirSync(file));
+
+  res.status(200).json({ files });
 }
