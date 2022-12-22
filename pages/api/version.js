75134eaca045ecd05a067bda3372727614708a96
Xelhua Corona <xelcoronap@gmail.com>
2022-12-22 13:48:16 -0600

    remove gs

--- pages/api/version.js
+++ pages/api/version.js
@@ -1,10 +1,6 @@
 import fs from "fs";
 
-import GS from "../../gs";
-
 export default async function handler(req, res) {
-  const gs = new GS();
-
   // version: await gs.version()
 
   res.status(200).json({ exists: fs.readdirSync("./libs/") });
