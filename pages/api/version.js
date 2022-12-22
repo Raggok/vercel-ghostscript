7794b88d93a524df50f7649dae8a145d8f0f1630
Xelhua Corona <xelcoronap@gmail.com>
2022-12-22 13:54:13 -0600

    explore current dir

--- pages/api/version.js
+++ pages/api/version.js
@@ -3,5 +3,5 @@ import fs from "fs";
 export default async function handler(req, res) {
   // version: await gs.version()
 
-  res.status(200).json({ exists: fs.readdirSync("./libs/") });
+  res.status(200).json({ exists: fs.readdirSync("./") });
 }
